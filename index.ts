ed55cf18dafd33ff36ec5e8f6042daea2bd72cd4
Fabiola <fabiola.aurelien@gmail.com>
2026-04-05 23:07:34 -0400

    Debugging for review failure to clg error message when ID i sout of bound

--- index.ts
+++ index.ts
@@ -9,13 +9,13 @@ fetchProductCatalog()
         console.log(data);
 
         // Get product reviews as long as catalogue is successfully retrieved
-        fetchProductReviews(2)
+        fetchProductReviews(3)
             .then((review) => {
                 console.log(review);
             })
             .catch((error) => {
                 console.log("An error occurred:", error);
-            });
+            })
 
         // Get sales report provided no failures in prior fetches
         fetchSalesReport()
@@ -24,9 +24,14 @@ fetchProductCatalog()
             })
             .catch((error) => {
                 console.error("An error occurred:", error);
-            });
+            })
     })
     // Issue error message for failed catalogue retrieval
     .catch((error) => {
         console.error("An error occurred:", error);
-    });
+    })
+
+    // Final log message indicating all calls have been attempted wether successful or not
+    .finally(() => {
+    console.log("All API calls attempted");
+});
